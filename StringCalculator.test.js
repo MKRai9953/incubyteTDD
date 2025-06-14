11518ddb487d25f29b5ac089285e6abeb6bf04ac
Mayank-Rai <mayank.dks@gmail.com>
2025-06-14 17:51:10 +0530

    added test case for adding number non-zero

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -7,3 +7,7 @@ test("returns 0 for an empty string", () => {
 test("returns number for a single value", () => {
   expect(add("1")).toBe(1);
 });
+
+test("adding two different number (non-zero)", () => {
+  expect(add("1,2")).toBe(3);
+});
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const { add } = require("./StringCalculator");
 
 console.log(add(""));
 console.log(add("1"));
+console.log(add("2", "3"));
