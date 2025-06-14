80ba471514e4768a19c8d36d15252bbc26459629
Mayank-Rai <mayank.dks@gmail.com>
2025-06-14 18:10:30 +0530

    added functionality for extra delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,15 +5,25 @@ function add(numbers) {
   let numberString = numbers;
 
   if (numbers.startsWith("//")) {
-    const [delimiterLine, ...rest] = numbers.split("\n");
-    const customDelimiter = delimiterLine.slice(2); //
-    delimiter = new RegExp(
-      customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
-    );
-    numberString = rest.join("\n");
+    const delimiterSectionEnd = numbers.indexOf("\n");
+    const delimiterLine = numbers.slice(2, delimiterSectionEnd);
+    numberString = numbers.slice(delimiterSectionEnd + 1);
+
+    const delimiterMatches = delimiterLine.match(/\[([^\]]+)\]/g);
+
+    if (delimiterMatches) {
+      const delimiters = delimiterMatches.map((d) =>
+        d.slice(1, -1).replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+      );
+      delimiter = new RegExp(delimiters.join("|"));
+    } else {
+      delimiter = new RegExp(
+        delimiterLine.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+      );
+    }
   }
 
-  const tokens = numberString.split(delimiter).map((n) => parseInt(n, 10));
+  const tokens = numberString.split(delimiter);
   const nums = tokens.map((n) => parseInt(n, 10)).filter((n) => !isNaN(n));
 
   const negatives = nums.filter((n) => n < 0);
@@ -21,7 +31,7 @@ function add(numbers) {
     throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
   }
 
-  return tokens.reduce((sum, num) => sum + num, 0);
+  return nums.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
