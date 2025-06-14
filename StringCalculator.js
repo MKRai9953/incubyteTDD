b5e9992a4ac0fd510af3c4e1a9b831b3e90623f3
Mayank-Rai <mayank.dks@gmail.com>
2025-06-14 18:04:20 +0530

    added negative numbers functionality

--- StringCalculator.js
+++ StringCalculator.js
@@ -14,6 +14,12 @@ function add(numbers) {
   }
 
   const tokens = numberString.split(delimiter).map((n) => parseInt(n, 10));
+  const nums = tokens.map((n) => parseInt(n, 10)).filter((n) => !isNaN(n));
+
+  const negatives = nums.filter((n) => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+  }
 
   return tokens.reduce((sum, num) => sum + num, 0);
 }
--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log(add("1"));
 console.log(add("2", "3"));
 console.log(add("1\n2,3"));
 console.log(add("//;\n1;2"));
+console.log(add("1,-2,3,-4"));
